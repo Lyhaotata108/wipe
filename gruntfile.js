2a4e9d63961c973577ef225a02c987b0a3acd7a1
Lyhaotata108 <1485926851@qq.com>
2018-11-16 09:32:47 +0800

    2018-11-16

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,18 @@
 //gruntfile.js
 //模块化导入函数
-module.exports=function(grunt){
+module.exports = function(grunt){
 	//所有插件的配置信息
 	grunt.initConfig({
-		// 获取package.json 
-		pkg:grunt.file.readJSON("package.json"),
+		//获取package.json 
+		pkg:grunt.file.readJSON('package.json'),
 		//gulify插件的配置信息
 		uglify:{
 			options:{
-				banner:'/*!<%= pkg.name %> <%= pkg.version %> 发布日期:<%= grunt.template.today("yyyy-mm-dd") %>*/',
+				banner:'/*!<%= pkg.name %> <%= pkg.version %> 发布日期：<%=grunt.template.today("yyyy-mm-dd")%>*/',
 			},
-			dist:{
+			build:{
 				src:"src/js/wipe.js",
-				dest:"dist/wipe-<%= pkg.version %>.min.js"
+				dest:"dist/js/wipe-<%= pkg.version %>.min.js"
 			},
 		},
 		cssmin:{
@@ -28,43 +28,42 @@ module.exports=function(grunt){
 					dest:'build/css',
 					ext:'.min.css'
 				}]
-			},
-			
+			}
 		},
 		clean:{
 			dest:['dist/*','sample/js/*']
 		},
-
 		jshint:{
-			test:["src/js/jquery.js"],
+			build:['src/js/wipe.js'],
 			options:{
-			       jshintrc:".jshintrc"	
+				jshintrc:'.jshintrc'
 			}
-		}
+		},
 		copy:{
 			js:{expand:true, cwd:'dist/js/',src:'*.min.js',dest:'sample/js/'}
 		},
-
-
-		// replace:{
-		// 	example:{
-		// 		src:["sample/index.html"],
-		// 		overwrite:true,
-		// 		replacements[{
-		// 			// from:/.min.js/g
-		// 		}]
-		// 	}
-		// }
-
+		replace:{
+			example:{
+				src:['sample/index.html'],
+				overwrite:true,
+				replacements:[{
+					from:/\d[\.]\d[\.]\d/g,
+					to:'<%= pkg.version %>'
+				},{
+					from:/hello\.css/g,
+					to:"hello.min.css"	
+				}]
+			}
+		}
 	});
 	//告诉grunt需要使用插件
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-cssmin");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-contrib-replace");
-	//告诉grunt当我们输入grunt命令后需要做些
-	//什么，有先后顺序
-	grunt.registerTask("default",["clean","jshint","cssmin","uglify","copy"]);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-text-replace');
+
+	//告诉grunt当我们输入grunt命令后需要做些什么，有先后顺序
+	grunt.registerTask('default',['jshint','clean','uglify','copy',"replace"]);
 };
--- src/js/wipe.js
+++ src/js/wipe.js
@@ -41,7 +41,7 @@ function drawLine(context,x1,y1,x2,y2){
 	context.restore();
 }
 function drawT(context,x1,y1,x2,y2){
-	if (arguments.length==3) {
+	if (arguments.length===3) {
 		context.save();
 		context.beginPath();
 		context.arc(x1,y1,radius,0,2*Math.PI);
@@ -50,7 +50,7 @@ function drawT(context,x1,y1,x2,y2){
 		context.stroke();
 		// 调用的是画点功能
 		
-	}else if(arguments.length==5){
+	}else if(arguments.length===5){
 		// 调用的是画线功能
 		context.save();
 		context.lineCap="round";
@@ -83,7 +83,7 @@ cas.addEventListener(clickEvtName,function(evt){
 // },false);
 // cas.addEventListener("mousemove",fn1,false);
 cas.addEventListener(moveEviName,function(evt){
-	if (insMouseDown==true){
+	if (insMouseDown===true){
 		var event = evt || window.event;
 		event.preventDefault();
 		//获取手指在视口的坐标，传递参数到drawPoint
@@ -115,7 +115,7 @@ cas.addEventListener(endEvtName,function(evt){
 	if (getTransparencyPercent(context)>50) {
 		alert("超过了50%的面积");
 		clearRect(context);
-	};
+	}
 	
 },false);
 function clearRect(context){
@@ -125,10 +125,10 @@ function getTransparencyPercent(context){
 	var t=context.getImageData(0,0,_w,_h);
 	for (var i = 0; i < t.data.length; i+=4) {
 			var c=t.data[i+3];
-			if (c==0) {
+			if (c===0) {
 				att++;
-			};
-		};
+			}
+		}
 		console.log("透明点的个数:"+att);
 		var percent=(att/(_w*_h))*100;
 		// console.log(percent);
